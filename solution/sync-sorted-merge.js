5028857b75c6bf8f8802297c970605502c17bdf3
Arshad-mohd <74669709+Arshad-mohd@users.noreply.github.com>
2024-03-23 22:52:01 +0530

    Update sync-sorted-merge.js

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,33 @@
-"use strict";
+class LogEntry {
+    constructor(sourceIndex, timestamp, message) {
+        this.sourceIndex = sourceIndex;
+        this.timestamp = timestamp;
+        this.message = message;
+    }
+}
 
-// Print all entries, across all of the sources, in chronological order.
+function drainLogsSynchronously(logSources) {
+    const minHeap = new MinHeap();
 
-module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
-};
+    
+    for (let i = 0; i < logSources.length; i++) {
+        const logSource = logSources[i];
+        const logEntry = logSource.pop(); 
+        if (logEntry !== false) {
+            minHeap.insert(new LogEntry(i, logEntry.date, logEntry.msg));
+        }
+    }
+
+    
+    while (!minHeap.isEmpty()) {
+        const earliestLogEntry = minHeap.extractMin();
+        console.log(`[${earliestLogEntry.timestamp}] ${earliestLogEntry.message}`);
+
+        const nextLogEntry = logSources[earliestLogEntry.sourceIndex].pop(); 
+        if (nextLogEntry !== false) {
+            minHeap.insert(new LogEntry(earliestLogEntry.sourceIndex, nextLogEntry.date, nextLogEntry.msg));
+        }
+    }
+}
+
+drainLogsSynchronously(logSources);
